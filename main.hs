af04bd91c515a770229b8d6f66cac615620d61d5
Noah Sehman <noah.sehman@gmail.com>
2024-08-23 12:07:11 -0700

    yay 600 commits wahoo

--- main.hs
+++ main.hs
@@ -1,17 +1,9 @@
 import System.Process (callCommand)
-import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
-    putStrLn "How many times do you want to commit?"
-    hFlush stdout
-    ipInput <- getLine
-    let ip = read ipInput :: Int
-    putStrLn "Auto git push when committed? (y/n)"
-    hFlush stdout
-    autoPush <- getLine
+    let ip = 600
     mapM_ (\i -> callCommand $ "git commit --allow-empty -m \"Commit " ++ show i ++ " of " ++ show ip ++ "\"") [1..ip]
     putStrLn $ "Committed " ++ show ip ++ " times"
-    if autoPush == "y"
-        then callCommand "git push"
-        else return ()
+    callCommand "git push"
+
