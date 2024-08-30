028b2d3bee48a19f46e700d15dbd90f1a47093e0
Noah Sehman <noah.sehman@gmail.com>
2024-08-30 11:47:28 -0700

    the automation wasnt working lmao so here we are

--- main.hs
+++ main.hs
@@ -1,9 +1,17 @@
 import System.Process (callCommand)
+import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
-    let ip = 600
+    putStrLn "How many times do you want to commit?"
+    hFlush stdout
+    ipInput <- getLine
+    let ip = read ipInput :: Int
+    putStrLn "Auto git push when committed? (y/n)"
+    hFlush stdout
+    autoPush <- getLine
     mapM_ (\i -> callCommand $ "git commit --allow-empty -m \"Commit " ++ show i ++ " of " ++ show ip ++ "\"") [1..ip]
     putStrLn $ "Committed " ++ show ip ++ " times"
-    callCommand "git push"
-
+    if autoPush == "y"
+        then callCommand "git push"
+        else return ()
